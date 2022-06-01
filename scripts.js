66510bbe7fce756027e65a7bb9ece8da4b7b4684
paulinemarg <paulinemargossian@gmail.com>
2022-06-01 22:29:12 +0200

    removed unnecessary images

--- scripts.js
+++ scripts.js
@@ -107,3 +107,18 @@ function showSlides(n) {
     }
     x[slideIndex - 1].style.display = "block";
 }
+
+// Project Tab scripts
+function openPage(pageName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
